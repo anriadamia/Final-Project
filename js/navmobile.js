9d630a1a469b1856203816d6a4bac81bf8d476c8
anriadamia <anriadamia2000@gmail.com>
2022-10-10 19:32:11 +0400

    Final Project

---
+++ js/navmobile.js
@@ -0,0 +1,11 @@
+function showburgermenu(){
+  document.getElementById("burgermenu").style.display="block";
+  document.getElementById("burger-icon").style.display="none";
+  document.getElementById("close-icon").style.display="block";
+}
+
+function hideburgermenu(){
+    document.getElementById("burgermenu").style.display="none";
+    document.getElementById("burger-icon").style.display="block";
+    document.getElementById("close-icon").style.display="none";
+}
